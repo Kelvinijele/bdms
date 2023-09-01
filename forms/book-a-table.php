a0c53f43604d019d6240a67130fe6805c6dbb3de
Emmanuel Kalu <kelvinijele@gmail.com>
2023-09-01 03:43:49 +0100

    worked on the form

--- forms/book-a-table.php
+++ forms/book-a-table.php
@@ -1,10 +1,4 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'kelvinijele@gmail.com';
@@ -21,7 +15,7 @@
   $book_a_table->to = $receiving_email_address;
   $book_a_table->from_name = $_POST['name'];
   $book_a_table->from_email = $_POST['email'];
-  $book_a_table->subject = "New table booking request from the website";
+  $book_a_table->subject = "New appointment request from the website";
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
